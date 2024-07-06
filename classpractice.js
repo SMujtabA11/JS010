8276a75cfc1306d6cb6a93b83dd553a3936b6260
SMujtabA11 <159699054+SMujtabA11@users.noreply.github.com>
2024-07-06 12:16:20 -0700

    Add files via upload

---
+++ classpractice.js
@@ -0,0 +1,33 @@
+// //Table print kry
+// var number = +prompt(`enter the number`);
+// for (var i = 1; i <= 10; i++) {
+//     console.log(`${i} * ${number} =`+number * i);
+// }
+
+// //Array element print k sum print kry
+// var myArray = []
+// var sum = 0;
+// for (var i = 0; i < 5; i++){
+// myArray.push(+prompt(`enter values`));
+// }
+// for (var j = 0; j < myArray.length; j++)
+// {
+//     sum = myArray[j] + sum ;
+// }
+// console.log(sum);
+
+// //User sy string ly kr usko ulta print kry
+// var myName = prompt(`enter the name`);
+// for (var i = myName.length; i >= 0; i--)
+// {
+//     console.log(myName[i]);
+// }
+
+//factorial
+// var number = +prompt(`enter the value`);
+// var fact = 1;
+// for (var i = 1; i <= number; i++){
+//     fact = i * fact;
+// }
+// console.log(fact);
+
